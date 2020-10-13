c40abdaa40c2e4e2a393171ba9378439dda52f2f
Dev Agrawal <devagrawal09@gmail.com>
2020-10-14 04:25:06 +0530

    added travis adapter and webhook config

--- adapters/adapter.ts
+++ adapters/adapter.ts
@@ -3,12 +3,14 @@ import { CardData } from '../card/card';
 import { GithubAdapter } from './github-adapter';
 import { HerokuAdapter } from './heroku-adapter';
 import { NetlifyAdapter } from './netlify-adapter';
+import { TravisAdapter } from './travis-adapter';
 import { UptimeRobotAdapter } from './uptime-robot-adapter';
 
 const adapters = {
   netlify: NetlifyAdapter,
   github: GithubAdapter,
   heroku: HerokuAdapter,
+  travis: TravisAdapter,
   uptimerobot: UptimeRobotAdapter
 };
 
---
+++ adapters/travis-adapter.ts
@@ -0,0 +1,58 @@
+import { APIGatewayEvent } from 'aws-lambda';
+import { Color } from '../card/card';
+import { Adapter } from './adapter';
+
+type BuildState = `Pending` | `Passed` | `Fixed` | `Broken` | `Failed` | `Still Failing` | `Canceled` | `Errored`;
+
+interface TravisData {
+  state: string
+  status_message: BuildState
+  result_message: BuildState
+  started_at: string
+  finished_at: string
+  duration: number,
+  build_url: string
+  branch: string
+  author_name: string
+  committer_name: string
+  committer_email: string
+  repository: {
+    name: string
+    owner_name: string
+  }
+};
+
+const stateToColor = (state: BuildState): Color => {
+  switch(state) {
+    case `Pending`:
+      return `accent`;
+    case `Passed`:
+    case `Fixed`:
+      return `good`;
+    default:
+      return `attention`;
+  };
+};
+
+const dateToEST = input => (new Date(input)).toLocaleString("en-US", { timeZone: "America/Atikokan" });
+
+export const TravisAdapter: Adapter = {
+  async parseRequest(e: APIGatewayEvent) {
+    const data: TravisData = JSON.parse(e.body);
+
+    return {
+      iconUrl: `https://secrethub.io/img/travis-ci.svg`,
+      title: {
+        text: `Build ${data.status_message}`,
+        color: stateToColor(data.result_message)
+      },
+      facts: [
+        { title: `Repo`, value: `${data.repository.owner_name}/${data.repository.name}` },
+        { title: `By`, value: data.committer_name },
+        { title: `Started at`, value: dateToEST(data.started_at) },
+        { title: `Finished at`, value: dateToEST(data.finished_at) },
+      ],
+      viewUrl: data.build_url
+    };
+  }
+}
